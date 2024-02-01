710969df02dc63ac3725acf69750bcf63aea5c4a
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2024-02-01 15:54:13 +0100

    Done

---
+++ Assets/Script/Ex_1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Ex_1 : MonoBehaviour
+{
+    private Coroutine _tempCoroutine;
+
+    [SerializeField]
+    private float _desiredTimeRotation = 5f;
+
+    public void CubeTurn()
+    {
+        _tempCoroutine = StartCoroutine(CubeTurnCoroutine());
+    }
+
+    public void StopCubeTurn()
+    {
+        StopCoroutine(_tempCoroutine);
+    }
+
+    private IEnumerator CubeTurnCoroutine()
+    {
+        float time = 0f;
+        while (true)
+        {
+            time += Time.deltaTime;
+            if (time >= _desiredTimeRotation)
+            {
+                yield break;
+            }
+
+            transform.Rotate(Vector3.up, 1f);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Script/Ex_2.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class Ex_2 : MonoBehaviour
+{
+    private CancellationTokenSource _token;
+    private bool _isSpinning = false;
+
+    [SerializeField]
+    private int _desiredTimeRotation = 5;
+
+    public void CubeTurn()
+    {
+        SpinCube();
+    }
+
+    public void StopCubeTurn()
+    {
+        _token.Cancel();
+        _token.Dispose();
+    }
+
+    private async void SpinCube()
+    {
+        _isSpinning = true;
+        _token = new CancellationTokenSource();
+        await UniTask.Delay(_desiredTimeRotation * 1000, cancellationToken: _token.Token).SuppressCancellationThrow();
+        _isSpinning = false;
+    }
+
+    private void Update()
+    {
+        if (_isSpinning)
+        {
+            transform.Rotate(Vector3.up, 1f);
+        }
+    }
+}
